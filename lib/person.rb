7645186d5898dabbbc504a261d91dc87972834d6
Felix <barosiofelix@gmail.com>
2022-10-18 16:13:01 +0300

    Completed Assignment

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+# require 'pry'
+
+class Person
+
+    def talk
+        puts "Hello World!"
+    end
+
+
+    def walk
+        puts "The Person is walking"
+    end
+    
+end
+
+
+john = Person.new
+brian = Person.new
+
+
+# binding.pry
+# 0
+
